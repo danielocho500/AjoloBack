e370462d4c5239efd823c5dfe565c5d31c874fab
Daniel Díaz <danielnochess@gmail.com>
2023-11-14 15:48:48 -0600

    getEmployeers

---
+++ controllers/user/getEmployeers.js
@@ -0,0 +1,47 @@
+const { verifyConnection } = require("../../db/verifyConnection");
+const { responseMsg } = require("../../helpers/responseMsg");
+const { responseServerError } = require("../../helpers/responseServerError");
+const { getUidByToken } = require("../../jwt/getUidByToken");
+const User = require("../../models/User");
+const { getUsers } = require("../../db/user/getUsers");
+const { where } = require("sequelize");
+
+const getEmployeers = async (req, res) => {
+  console.log("Get Employeers");
+
+  const isConnected = await verifyConnection();
+  if (!isConnected) {
+    return responseServerError(res);
+  }
+
+  const uuid = getUidByToken(req.headers.authtoken);
+
+  const user = await User.findOne({ where: { uuid } });
+  if (!user) {
+    return responseMsg(res, 401, "fail", "Not user Found", {
+      qrCode: false,
+    });
+  }
+
+  if(user.id_rol != 2){
+    return responseMsg(res, 401, "fail", "Not authorized", {
+        logged: false,
+      });
+  }
+
+  const users = User.findAll({
+    where: {
+      id_rol: {
+        [Op.or]: [3, 4] // Utilizando Sequelize (suponiendo que estás utilizando Sequelize)
+      }
+    }
+  });
+
+  return responseMsg(res, 200, 'success', 'Users obtained', {
+    users,
+  })
+};
+
+module.exports = {
+  getEmployeers,
+};
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,9 @@ const { validateParams } = require("../helpers/validateParams");
 const { validateJWT } = require("../jwt/validateJWT");
 const { check } = require("express-validator");
 const { getUsersByRol } = require("../controllers/user/getUsersByRol");
+const { getEmployeers } = require("../controllers/user/getEmployeers")
 const { uploadImage } = require("../controllers/user/uploadImage");
+const { getStream } = require('firebase/storage');
 
 const upload = multer({ storage: multer.memoryStorage() });
 
@@ -16,6 +18,15 @@ router.get(
     validateJWT
   ],
   getUsersByRol
+);
+
+router.get(
+  "/employeers",
+  [
+    validateParams,
+    validateJWT
+  ],
+  getEmployeers
 );
 
 router.post(
